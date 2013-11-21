05dca42854718de4e0c71d559691f5ae9c53baa5
Jayne Vidheecharoen <jaynev@gmail.com>
2013-11-21 12:13:10 -0800

    removed year from search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
 	def self.search_for( query )
-		Movie.where("title LIKE :query OR description LIKE :query OR year_released LIKE :query", :query => "%#{query}%")
+		Movie.where("title LIKE :query OR description LIKE :query", :query => "%#{query}%")
 	end	
 end
