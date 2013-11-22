ad08c8367927402fff703f32b2d4481a5e825fdf
Jayne Vidheecharoen <jaynev@gmail.com>
2013-11-21 17:54:27 -0800

    took out the movie poster

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,12 +3,12 @@ class Movie < ActiveRecord::Base
 		Movie.where("title LIKE :query OR description LIKE :query", :query => "%#{query}%")
 	end	
 
-	before_save :fill_poster
+	# before_save :fill_poster
 
-	def fill_poster
-		title = URI::encode(self.title)
-		omdb_json = RestClient.get("http://www.omdbapi.com/?i=&t=#{title}")
-		omdb_movie = JSON.load(omdb_json)
-		self.poster = omdb_movie["Poster"]
-	end
+	# def fill_poster
+	# 	title = URI::encode(self.title)
+	# 	omdb_json = RestClient.get("http://www.omdbapi.com/?i=&t=#{title}")
+	# 	omdb_movie = JSON.load(omdb_json)
+	# 	self.poster = omdb_movie["Poster"]
+	# end
 end
